b0c93d1885d270568691b0ea785fce9c2d7d9fc4
josifchin75 <josifchin75@gmail.com>
2016-12-23 08:17:25 -0500

    added out of network share code

---
+++ MyFileItPEService/ViewDocument.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MyFileItPE.Master" AutoEventWireup="true" CodeBehind="ViewDocument.aspx.cs" Inherits="MyFileItPEService.ViewDocument" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div>
+        <asp:Label ID="lblDocumentInfo" runat="server"></asp:Label>
+        <asp:Image ID="imgMain" runat="server" />
+    </div>
+</asp:Content>
---
+++ MyFileItPEService/ViewDocument.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFileItPEService
+{
+    public partial class ViewDocument : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            setupPage();
+        }
+        protected void setupPage()
+        {
+            var guid = Request.Params["g"].ToString();
+            lblDocumentInfo.Text = guid;
+            //check for data
+            var svc = new MyFileItPEMainService();
+            var result = svc.GetEmergencyShare(ConfigurationSettings.ServiceUser, ConfigurationSettings.ServicePass, guid);
+
+            if (result.Success)
+            {
+                var d = result.Documents.First();
+                imgMain.Visible = true;
+                imgMain.ImageUrl = "data:image/png;base64," + d.Base64Image;
+                lblDocumentInfo.Text = "<h2>" + d.FIRSTNAME + " " + d.LASTNAME + "</h2>" + "<h3>" + d.DocumentTypeName + "<h3>" + "<i>" + d.COMMENT + "</i><p></p>";
+            }
+            else
+            {
+                lblDocumentInfo.Text = "This document is invalid or expired.";
+                imgMain.Visible = false;
+            }
+        }
+    }
+}
